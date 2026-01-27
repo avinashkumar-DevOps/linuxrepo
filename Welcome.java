5892fe6bceafdf5544ed74fb59080a3f36a07657
Avinash Kumar <avinash.meet@gmail.com>
2026-01-27 16:45:54 +0100

    Second commit

--- Welcome.java
+++ Welcome.java
@@ -1,5 +1,6 @@
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("welcome to my first repository");
+        System.out.println("This  is my repository to store Java projects");
     }
 }
