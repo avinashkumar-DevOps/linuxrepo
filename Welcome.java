e62fd3af124fc5db9dfc9302458445a4b2f315d4
avinashkumar-DevOps <avinash.meet@gmail.com>
2026-01-27 19:23:28 +0100

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -2,5 +2,7 @@ public class Welcome {
     public static void main(String[] args) {
         System.out.println("welcome to my first repository");
         System.out.println("This  is my repository to store Java projects");
+        System.out.println("My Java Program");
+         System.out.println("This  is my repository to store Java projects");
     }
 }
